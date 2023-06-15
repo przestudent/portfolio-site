1f0a531c2d6699d7ecf1c5465f7ab536755d36f0
3 <przemekpalac@student.agh.edu.pl>
2023-06-15 11:22:27 +0200

    RESPONSIVNES, IMAGES, DONE
    also vite

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
     });
   });
 });
@@ -18,14 +18,37 @@ function isInViewport(el) {
   );
 }
 
-const divider = document.querySelector(".divider");
-const message = document.querySelector(".divider>span");
+const divider = document.querySelector('.divider');
+const message = document.querySelector('.divider>span');
 function ScrollAnimation() {
   if (isInViewport(divider)) {
-    divider.classList.add("divider-grow");
-    document.removeEventListener("scroll", ScrollAnimation);
+    divider.classList.add('divider-grow');
+    document.removeEventListener('scroll', ScrollAnimation);
   }
 }
-document.addEventListener("scroll", ScrollAnimation, {
+document.addEventListener('scroll', ScrollAnimation, {
   passive: true,
 });
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show-slide-sideways');
+    } else {
+      entry.target.classList.remove('show-slide-sideways');
+    }
+  });
+});
+document
+  .querySelectorAll('.hidden-sideways')
+  .forEach((e) => observer.observe(e));
+
+const observerNavBar = new IntersectionObserver((navBar) => {
+  if (navBar[0].isIntersecting) {
+    navBar[0].target.classList.add('show-slide-down');
+  } else {
+    navBar[0].target.classList.remove('show-slide-down');
+  }
+});
+console.log(document.querySelector('nav'));
+observerNavBar.observe(document.querySelector('nav'));
