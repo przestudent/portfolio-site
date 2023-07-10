e5de58a5ffa4e887d6e95544278152d4ee617792
3 <przemekpalac@student.agh.edu.pl>
2023-07-10 08:47:43 +0200

    Intersection observers

--- src/index.js
+++ src/index.js
@@ -7,47 +7,36 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-function isInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
 const divider = document.querySelector('.divider');
-const message = document.querySelector('.divider>span');
-function ScrollAnimation() {
-  if (isInViewport(divider)) {
-    divider.classList.add('divider-grow');
-    document.removeEventListener('scroll', ScrollAnimation);
-  }
-}
-document.addEventListener('scroll', ScrollAnimation, {
-  passive: true,
-});
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show-slide-sideways');
-    } else {
-      entry.target.classList.remove('show-slide-sideways');
-    }
-  });
-});
+const dividerObserver = new IntersectionObserver(
+  interSectionCallback('divider-grow', false)
+);
+dividerObserver.observe(divider);
+
+const observer = new IntersectionObserver(
+  interSectionCallback('show-slide-sideways', false)
+);
 document
   .querySelectorAll('.hidden-sideways')
   .forEach((e) => observer.observe(e));
 
-const observerNavBar = new IntersectionObserver((navBar) => {
-  if (navBar[0].isIntersecting) {
-    navBar[0].target.classList.add('show-slide-down');
-  } else {
-    navBar[0].target.classList.remove('show-slide-down');
-  }
-});
+const observerNavBar = new IntersectionObserver(
+  interSectionCallback('show-slide-down')
+);
 observerNavBar.observe(document.querySelector('nav'));
+
+function interSectionCallback(classEl, repeat = true) {
+  return function (navBar) {
+    navBar.forEach((nav) => {
+      if (nav.isIntersecting) {
+        nav.target.classList.add(classEl);
+        if (!repeat) {
+          this.unobserve(nav.target);
+        }
+      } else if (repeat) {
+        nav.target.classList.remove(classEl);
+      }
+    });
+  };
+}
